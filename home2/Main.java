288371ede92198e16dc07a54e80416aa7bb471a4
Murat Kamaletdinov <kamaletdinov2425@icloud.com>
2020-10-08 22:48:43 +0300

    Fixed some bugs, added 1 problem

--- home2/Main.java
+++ home2/Main.java
@@ -21,7 +21,14 @@ public class Main {
         System.out.println();
         int find = in.nextInt();
         System.out.println(binary_search(array, find));
+        int[] arr = {51, 235, 58, 92};
+        int low = 0;
+        int high = arr.length - 1;
+        System.out.println(Arrays.toString(arr));
+        quickSort(arr, low, high);
+        System.out.println(Arrays.toString(arr));
 
+        System.out.println(strSort("acd abc")); //str sorting
     }
 
     static ArrayList<Integer> mergesort(ArrayList<Integer> array) {
@@ -64,7 +71,48 @@ public class Main {
         }
         return -1;
     }
-    static String str_sort(String string){
-        
+
+    static String strSort(String string){
+        String[] arr = string.split(" ");
+        List<String> array  = Arrays.asList(arr);
+        Collections.sort(array);
+        String result = String.join(" ", array);
+        return result;
+    }
+
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (array.length == 0)
+            return;
+        if (low >= high)
+            return;
+
+        int middle = low + (high - low) / 2;
+        int pivot = array[middle];
+        int i = low, j = high;
+
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+
+            while (array[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (low < j){
+            quickSort(array, low, j);
+        }
+        if (high > i) {
+            quickSort(array, i, high);
+        }
     }
 }
