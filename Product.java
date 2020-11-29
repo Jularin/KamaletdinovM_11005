52578f66fe23818a857401945d2d498d63c343f6
Jularin <58310353+Jularin@users.noreply.github.com>
2020-11-29 19:16:28 +0300

    New homework
    Added new homework

---
+++ Product.java
@@ -0,0 +1,13 @@
+package home7;
+
+public class Product {
+    private String name;
+    private double price;
+    private String manufacturer;
+
+    public Product(String name, double price, String manufacturer){
+        this.name = name;
+        this.price = price;
+        this.manufacturer = manufacturer;
+    }
+}
